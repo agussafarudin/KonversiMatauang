57c2faf0fe4c0ddb8e0ca2708583024f0b3afec8
agussafarudin <shempal.sf@gmail.com>
2019-01-04 00:59:20 +0700

    v-01

---
+++ KonversiMatauang/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float Jumlah,Rupiah,Dollar;
+    cout<<"------Konversi Mata Uang Dollar ke Rupiah-------\n \n";
+    cout<<"Jika 1 Dollar maka    = ";
+    cin>>Rupiah;
+    cout<<"\n";
+
+    cout<<"Masukkan Nilai Dollar = ";
+    cin>>Dollar;
+    cout<<"\n";
+    cout<<"Maka nilainya adalah  = ";
+    cout<<(Rupiah*Dollar);
+    cout<<" Rupiah";
+    return 0;
+}
